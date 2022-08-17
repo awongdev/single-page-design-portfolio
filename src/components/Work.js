28b47145871161640f5c780f21f6ab318b9c037c
Aaron W <26586148+AceDub@users.noreply.github.com>
2022-08-16 17:13:08 -0700

    fix image slider

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,7 +3,7 @@ import Carousel from '../UI/Carousel';
 
 const Work = () => {
   return (
-    <section className="mx-auto mb-24 flex max-w-[90rem] justify-center overflow-hidden xl:mb-20">
+    <section className="mx-auto mb-24 flex  max-w-[103.125rem] justify-center overflow-hidden xl:mb-20">
       <div>
         <h2 className="mb-8 text-center text-2xl font-bold md:mb-14 md:text-[2rem]">
           My Work
